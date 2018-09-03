df64cc69aa5e543b6577d0541ed5c5ecd241f685
Aaron <aaronliu12@gmail.com>
2018-09-03 20:54:12 +0800

    commit

--- js/index.js
+++ js/index.js
@@ -51,12 +51,15 @@ function rolebutton4(){
     
 }
 
+/* Inspiration for this and other hover effects - https://codepen.io/shshaw/pen/PQbQgE */
+/* Check out SplittingJS -
+https://splitting.js.org */
+
+
+
 Splitting({
 	target: "[data-rows], [data-columns], [data-image]",
 	by: "cells",
 	image: true
 });
 
-/* Inspiration for this and other hover effects - https://codepen.io/shshaw/pen/PQbQgE */
-/* Check out SplittingJS -
-https://splitting.js.org */
